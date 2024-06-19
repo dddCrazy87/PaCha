3402ed9beac5afe790ee0141b8fe212c72422f28
dddCrazy87 <n126970@gmail.com>
2024-06-20 02:36:13 +0800

    Add parking lot detail view to home view

--- Home/ParkingNavView.swift
+++ Home/ParkingNavView.swift
@@ -1,113 +1,134 @@
 import SwiftUI
 
 struct ParkingNavView: View {
+    
+    @Binding var parkingLotData: ParkingLotData?
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .frame(width: 350, height: 315)
-                .foregroundColor(.white)
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            
-            VStack {
-                HStack {
-                    Text("88房(XX大道 8路-9路)停車場")
-                        .font(.title3)
-                        .bold()
-                        .padding(.trailing, 5)
-                    
-                    Image(systemName: "heart")
-                        .resizable()
-                        .frame(width: 27, height: 22)
-                }
-                HStack {
-                    Image("")
-                        .resizable()
-                        .frame(width: 15, height: 17)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    
-                    Text("40/hr")
-                        .bold()
-                    
-                    Rectangle()
-                        .frame(width: 1, height: 24)
-                        .foregroundColor(.orange)
-                    
-                    Image("")
-                        .resizable()
-                        .frame(width: 15, height: 17)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    
-                    Text("30")
-                        .bold()
-                    
-                    Rectangle()
-                        .frame(width: 1, height: 24)
-                        .foregroundColor(.orange)
-                    
-                    Image("")
-                        .resizable()
-                        .frame(width: 15, height: 17)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    
-                    Text("0.3公里")
-                        .bold()
-                }
+        if let parkingLotData {
+            ZStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 350, height: 315)
+                    .foregroundColor(.white)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                 
-                Rectangle()
-                    .frame(width: 312, height: 2)
-                    .foregroundColor(.orange)
-                    .padding(.vertical, 10)
-                
-                HStack {
-                    
-                    Image("")
-                        .resizable()
-                        .frame(width: 125, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    
-                    VStack {
+                VStack {
+                    HStack {
+                        Text(parkingLotData.name)
+                            .font(.title3)
+                            .bold()
+                            .padding(.leading, 50)
                         
-                        HStack(alignment: .top) {
-                            
-                            Text("地址：")
-                                .padding(0)
-                            
-                            Spacer().frame(width: 0)
-                            
-                            Text("A市OO區XX大道二段18號")
-                                .frame(width: 130)
-                        }
+                        Spacer()
                         
-                        HStack(alignment: .top) {
-                            
-                            Text("計費：")
-                                .padding(0)
-                            
-                            Spacer().frame(width: 0)
-                            
-                            Text("40元/時,全程以半小時計費.月租:全日12.000元/月")
-                                .frame(width: 130)
-                        }
+                        Image(systemName: "heart")
+                            .resizable()
+                            .frame(width: 27, height: 22)
+                            .padding(.trailing, 50)
                     }
-                    .font(.caption)
-                    .padding(.leading, 4)
-                }
-                
-                Button {
-                    
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 30)
-                            .frame(width: 285, height: 42)
+                    HStack {
+                        Image("Price")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                        
+                        Text("40/hr")
+                            .bold()
+                        
+                        Rectangle()
+                            .frame(width: 1, height: 24)
                             .foregroundColor(.orange)
+                        
+                        Image("Space")
+                            .resizable()
+                            .frame(width: 25, height: 17)
+                        
+                        Text("30")
+                            .bold()
+                        
+                        Rectangle()
+                            .frame(width: 1, height: 24)
+                            .foregroundColor(.orange)
+                        
+                        Image("Distance")
+                            .resizable()
+                            .frame(width: 12, height: 15)
+                        
+                        Text("0.3公里")
+                            .bold()
                     }
+                    
+                    Rectangle()
+                        .frame(width: 312, height: 2)
+                        .foregroundColor(.orange)
+                        .padding(.vertical, 10)
+                    
+                    HStack {
+                        
+                        Image("")
+                            .resizable()
+                            .frame(width: 125, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                        
+                        VStack {
+                            
+                            HStack(alignment: .top) {
+                                
+                                Text("地址：")
+                                    .padding(0)
+                                
+                                Spacer().frame(width: 0)
+                                
+                                Text(parkingLotData.address)
+                                    .frame(width: 130)
+                                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                            }
+                            
+                            Spacer().frame(height: 5)
+                            
+                            HStack(alignment: .top) {
+                                
+                                Text("計費：")
+                                    .padding(0)
+                                
+                                Spacer().frame(width: 0)
+                                
+                                Text(parkingLotData.payex)
+                                    .frame(width: 130)
+                                    .lineLimit(3)
+                            }
+                        }
+                        .font(.caption)
+                        .padding(.leading, 4)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 30)
+                                .frame(width: 285, height: 42)
+                                .foregroundColor(.orange)
+                            
+                            Image("Navigation")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                        }
+                    }
+                    .padding(5)
                 }
-                .padding(5)
             }
         }
     }
 }
 
+struct ParkingNavViewTest:View {
+    @State private var test:ParkingLotData?
+    
+    var body: some View {
+        ParkingNavView(parkingLotData: $test)
+    }
+}
+
 #Preview {
-    ParkingNavView()
+    ParkingNavViewTest()
 }
