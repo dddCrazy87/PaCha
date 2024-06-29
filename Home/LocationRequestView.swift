bb0cbb610e13ddbe6fb699a2cc487de3a2765c75
dddCrazy87 <n126970@gmail.com>
2024-06-30 07:20:46 +0800

    Modify style

--- Home/LocationRequestView.swift
+++ Home/LocationRequestView.swift
@@ -1,22 +1,9 @@
-//
-//  LocationRequestView.swift
-//  PaCha
-//
-//  Created by 許昀韋 on 2024/6/18.
-//
-
 import SwiftUI
 
 struct LocationRequestView: View {
     var body: some View {
         VStack {
             Text("請先開啟定位！")
-            
-            Button {
-                LocationManager.shared.requstLocation()
-            } label: {
-                Text("同意")
-            }
         }
         
     }
